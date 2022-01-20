e1c24cd2d068383931b8379b0754fd081d50a649
Rolin Bos <s1145536@student.windesheim.nl>
2022-01-20 18:03:57 +0100

    Console done

--- RaceSimulator/Program.cs
+++ RaceSimulator/Program.cs
@@ -10,6 +10,16 @@ namespace RaceSimulator
     {
         static void Main(string[] args)
         {
+            Competition competition = new Competition();
+            Data.Initialize(competition);
+            Data.NextRace();
+            Visualisation.Initialize();
+            Visualisation.DrawTrack(Data.CurrentRace.Track);
+
+            for (; ; )
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
